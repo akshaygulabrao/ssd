af80ede45101b1c813d322fd244057bdf75fe159
Akshay <aksgula22@gmail.com>
2023-05-28 18:31:42 -0400

    backbone added, head started

---
+++ backbone.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+class Backbone(nn.Module):
+    def __init__(self):
+        super(Backbone, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding='same')
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding='same')
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding='same')
+        self.bn3 = nn.BatchNorm2d(256)
+        self.relu3 = nn.ReLU()
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding='same')
+        self.bn4 = nn.BatchNorm2d(512)
+        self.relu4 = nn.ReLU()
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv5 = nn.Conv2d(512, 512, kernel_size=3, padding='same')
+        self.bn5 = nn.BatchNorm2d(512)
+        self.relu5 = nn.ReLU()
+        self.maxpool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+    def forward(self, x):
+        outputs = []
+        x = self.maxpool1(self.relu1(self.bn1(self.conv1(x))))
+        x = self.maxpool2(self.relu2(self.bn2(self.conv2(x))))
+        x = self.maxpool3(self.relu3(self.bn3(self.conv3(x))))
+        outputs.append(x.clone())
+        x = self.maxpool4(self.relu4(self.bn4(self.conv4(x))))
+        outputs.append(x.clone())
+        x = self.maxpool5(self.relu5(self.bn5(self.conv5(x))))
+        outputs.append(x.clone())
+        return outputs
+
+random_img = torch.randn((1,3,512,512))
+backbone = Backbone()
+out = backbone(random_img)
+for i in out:
+    print(i.shape)
---
+++ head.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+class SSDHead(nn.Module):
+    def __init__(self, num_classes, num_anchors):
+        super(SSDHead, self).__init__()
+
+        self.num_classes = num_classes
+        self.num_anchors = num_anchors
+
+        self.classification_layers = nn.ModuleList([
+            nn.Conv2d(512, num_anchors * num_classes, kernel_size=3, padding=1),
+            nn.Conv2d(512, num_anchors * num_classes, kernel_size=3, padding=1),
+            nn.Conv2d(512, num_anchors * num_classes, kernel_size=3, padding=1)
+        ])
+
+        self.regression_layers = nn.ModuleList([
+            nn.Conv2d(512, num_anchors * 4, kernel_size=3, padding=1),
+            nn.Conv2d(512, num_anchors * 4, kernel_size=3, padding=1),
+            nn.Conv2d(512, num_anchors * 4, kernel_size=3, padding=1)
+        ])
+
+    def forward(self, features):
+        class_predictions = []
+        box_regression = []
+
+        for feature, cls_layer, reg_layer in zip(features, self.classification_layers, self.regression_layers):
+            class_predictions.append(cls_layer(feature).permute(0, 2, 3, 1).contiguous())
+            box_regression.append(reg_layer(feature).permute(0, 2, 3, 1).contiguous())
+
+        class_predictions = torch.cat([pred.view(pred.size(0), -1, self.num_classes) for pred in class_predictions], dim=1)
+        box_regression = torch.cat([reg.view(reg.size(0), -1, 4) for reg in box_regression], dim=1)
+
+        return class_predictions, box_regression
